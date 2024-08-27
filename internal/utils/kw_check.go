9b82b88877ff43195fa4226e77e8c00b4ce24f23
Wen Junhua <wenjh1998@gmail.com>
2024-08-27 23:36:11 +0800

    refactor: update naming

---
+++ internal/utils/kw_check.go
@@ -0,0 +1,12 @@
+package utils
+
+import "github.com/Jh123x/buildergen/internal/consts"
+
+func IsKeyword(s string) bool {
+	switch s {
+	case consts.KEYWORD_GO, consts.KEYWORD_IF, consts.KEYWORD_FOR, consts.KEYWORD_MAP, consts.KEYWORD_VAR, consts.KEYWORD_BREAK, consts.KEYWORD_CASE, consts.KEYWORD_CHAN, consts.KEYWORD_ELSE, consts.KEYWORD_FUNC, consts.KEYWORD_GOTO, consts.KEYWORD_TYPE, consts.KEYWORD_CONST, consts.KEYWORD_DEFER, consts.KEYWORD_RANGE, consts.KEYWORD_RETURN, consts.KEYWORD_SELECT, consts.KEYWORD_STRUCT, consts.KEYWORD_SWITCH, consts.KEYWORD_IMPORT, consts.KEYWORD_DEFAULT, consts.KEYWORD_PACKAGE, consts.KEYWORD_CONTINUE, consts.KEYWORD_INTERFACE, consts.KEYWORD_FALLTHROUGH:
+		return true
+	default:
+		return false
+	}
+}
