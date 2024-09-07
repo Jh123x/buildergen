1e57cea03f599b131f59c23a6e048f6a0e619d02
Wen Junhua <wenjh1998@gmail.com>
2024-09-08 02:54:31 +0800

    feat: update benchmark

--- examples/benchmark/bench_test.go
+++ examples/benchmark/bench_test.go
@@ -10,22 +10,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var config = &cmd.Config{
-	Source:      "./benchmark.go",
-	Destination: "./benchmark_builder.go",
-	Package:     "benchmark",
-	Name:        "Data",
-}
+var (
+	config = &cmd.Config{
+		Source:      "./benchmark.go",
+		Destination: "./benchmark_builder.go",
+		Package:     "benchmark",
+		Name:        "Data",
+	}
+
+	expectedRes, _ = os.ReadFile("./benchmark_builder.go")
+)
 
 func BenchmarkCodeGen(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		data, err := parser.ParseBuilderFile(config)
 		assert.Nil(b, err)
 
-		file, err := os.Create(config.Destination)
-		assert.Nil(b, err)
-
-		file.WriteString(data)
-		assert.Nil(b, file.Close())
+		assert.Equal(b, string(expectedRes), data)
 	}
 }
