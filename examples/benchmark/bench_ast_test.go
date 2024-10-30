b2e4ba8d400be1f81367c9bacf81af25733ec6cb
Wen Junhua <wenjh1998@gmail.com>
2024-10-30 17:47:11 +0800

    feat: update benchmark

---
+++ examples/benchmark/bench_ast_test.go
@@ -0,0 +1,42 @@
+// The main package for running struct generation benchmarks
+package benchmark
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Jh123x/buildergen/internal/cmd"
+	"github.com/Jh123x/buildergen/internal/consts"
+	"github.com/Jh123x/buildergen/internal/parser"
+	"github.com/stretchr/testify/assert"
+)
+
+var astConfig = &cmd.Config{
+	Source:      "./benchmark.go",
+	Destination: "./benchmark_builder.go",
+	Package:     "benchmark",
+	Name:        "Data",
+	ParserMode:  consts.MODE_AST,
+}
+
+func BenchmarkASTCodeGen(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		data, err := parser.ParseBuilderFile(astConfig)
+		assert.Nil(b, err)
+
+		assert.Equal(b, string(expectedRes), data)
+	}
+}
+
+func BenchmarkASTCodeGenWithIO(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		data, err := parser.ParseBuilderFile(astConfig)
+		assert.Nil(b, err)
+
+		file, err := os.Create(astConfig.Destination)
+		assert.Nil(b, err)
+
+		file.WriteString(data)
+		assert.Nil(b, file.Close())
+	}
+}
