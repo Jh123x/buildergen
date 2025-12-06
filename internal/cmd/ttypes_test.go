a1c09620cab95c0bd03b3b65a2fd367410bb6764
Wen Junhua <wenjh1998@gmail.com>
2025-12-06 18:20:48 +0800

    chore: update go generate cmd

--- internal/cmd/ttypes_test.go
+++ internal/cmd/ttypes_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-//go:generate buildergen -src ttypes_test.go -name testCase -dst ttypes_builder_test.go
+//go:generate buildergen --src=ttypes_test.go --name=testCase --dst=ttypes_builder_test.go
 
 type testCase struct {
 	src            string
