9652de3670997e093191ffa1903c96c8caf5d2f2
Wen Junhua <wenjh1998@gmail.com>
2025-12-21 01:06:02 +0800

    test: update test case

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -68,13 +68,14 @@ func TestParseBuilderFile(t *testing.T) {
 		},
 	}
 
-	currDir, err := os.Getwd()
+	_, err := os.Getwd()
 	assert.Nil(t, err)
 
 	for name, tc := range tests {
 		if tc.config != nil {
-			tc.config.Source = filepath.Join(currDir, tc.config.Source)
-			tc.expectedFileRes = filepath.Join(currDir, tc.expectedFileRes)
+			tc.config.Source = filepath.Join(
+				tc.config.Source)
+			tc.expectedFileRes = filepath.Join(tc.expectedFileRes)
 		}
 
 		t.Log(tc.config.Source)
